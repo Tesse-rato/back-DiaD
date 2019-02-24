fc632a52a40f41132707a44de278904d475daaf9
unknown <xurask8@gmail.com>
2019-02-24 19:57:57 -0300

    Refazendo API com express

--- dist/ambiente/ambiente.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.enviroment = {
-    server: { port: process.env.SERVER_PORT || 421 },
-    db: { url: process.env.DB_URL || 'mongodb://localhost/dora-api' }
-};
